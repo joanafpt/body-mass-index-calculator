b65cb0e50eff308b77413ca43984c1332be6b097
joanafpt <joanajteixeira@outlook.com>
2020-02-11 13:12:02 +0000

    bmi calc

---
+++ precache-manifest.71d7a63cf691a0c07ed8bde7cc671946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901b6c222697208a3c6ac585a64f9464",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cf0533e819af04a7e910",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5cbf0220b4a98f02cd22",
+    "url": "./static/css/main.53dd86d8.chunk.css"
+  },
+  {
+    "revision": "cf0533e819af04a7e910",
+    "url": "./static/js/2.27dea20a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.27dea20a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5cbf0220b4a98f02cd22",
+    "url": "./static/js/main.2ae9f6a7.chunk.js"
+  },
+  {
+    "revision": "12ffc9a55ad7c713faeb",
+    "url": "./static/js/runtime-main.4a80a9d9.js"
+  }
+]);
